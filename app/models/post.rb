33e07f02fcd6e53e80969a43845bbb7677d39788
iamcid <iamcid17@gmail.com>
2020-09-30 23:09:04 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :click_bait
+
+    def click_bait
+        if title && !title.include?("Won't Believe" || "Secret" || "Top" || "Guess")
+            errors.add(:title, "This isn't clickbait!")
+        end
+    end
 end
