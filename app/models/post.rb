1e7a59763a41909845577999b7184422b4d929df
Jorge Ramirez <joramirez96@gmail.com>
2020-10-16 03:26:48 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
+require 'pry'
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :must_be_clickbait
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+
+  def must_be_clickbait
+    if CLICKBAIT_PATTERNS.none?{|p| p.match title}
+      errors.add(:title, "must_be_clickbait")
+    end
+  end
+
 end
