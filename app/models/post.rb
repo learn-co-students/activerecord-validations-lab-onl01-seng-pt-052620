7e8e5f01ca4ed999569d83bddc826622db89a403
Capsulecoding <capsulecoding@icloud.com>
2020-10-19 10:40:41 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :summary, length: { maximum: 250 }
+    validates :context, length: { minimum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validates_with TitleValidator
 end
