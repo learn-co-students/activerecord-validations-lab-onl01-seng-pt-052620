69905baf713d01c1f5e9813157a933791b272294
edmundo papi <edmundopierre@yahoo.com>
2020-10-23 02:43:50 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+ include ActiveModel::Validations
+  validates :title, presence: true
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validates :summary, length: {maximum: 250}
+  validates :content , length: {minimum:250 }
+    validates_with MyValidator, on: :create
+
 end
