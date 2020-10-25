90ac3fac31f0b00f02f165aa45749da618d5b1d4
rhaynes3k <rhaynes3k@gmail.com>
2020-10-25 15:28:58 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
+  validates(:content, {:length => {:minimum => 250}})
+  validates(:summary, {:length => {:maximum => 250}})
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+  validate :title_is_clickbate
+
+    def title_is_clickbate
+      title_is_clickbate.includes?("Guess")
+    end
 end
